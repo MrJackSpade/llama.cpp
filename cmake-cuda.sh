783527e20f9170bbb92707d9d858648553c58404
Anthony Loffredo <loffredo.ap@gmail.com>
2024-07-18 22:26:13 -0700

    updating build script

--- cmake-cuda.sh
+++ cmake-cuda.sh
@@ -1,4 +1,14 @@
-cmake -S . -B build/ -D CMAKE_BUILD_TYPE=x64-linux-cuda-static -DGGML_CUDA=ON
-cmake --build build/ --config x64-linux-cuda-static
+cmake -S . -B build/ \
+    -D CMAKE_BUILD_TYPE=Release \
+    -DGGML_CUDA=ON \
+    -DGGML_AVX=ON \
+    -DGGML_AVX2=ON \
+    -DGGML_AVX512=ON \
+    -DGGML_AVX512_VBMI=ON \
+    -DGGML_AVX512_VNNI=ON \
+    -DGGML_FMA=ON \
+    -DGGML_F16C=ON
+
+cmake --build build/ -- -j 8
 sudo find / | grep libggml.so
 sudo find / | grep libllama.so
