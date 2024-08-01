945b3682e7fb85913cc7536e73dbd4533a50b63e
MrJackSpade <loffredo.ap@gmail.com>
2024-07-31 23:34:42 -0700

    scripts

--- update_llamabot_cpu.sh
+++ update_llamabot_cpu.sh
@@ -1,9 +1,9 @@
-cp build/ggml/src/libggml.so ../LlamaBot/LlamaNative/Binaries/CPU/libggml.so
-rm ../LlamaBot/LlamaNative/Binaries/CPU/libggml.so.zip
-rm ../LlamaBot/LlamaNative/Binaries/CPU/libggml.so.z01
-cp build/src/libllama.so ../LlamaBot/LlamaNative/Binaries/CPU/libllama.so
-rm ../LlamaBot/LlamaNative/Binaries/CPU/libllama.so.zip
-cd ../LlamaBot/LlamaNative/Binaries/CPU
+cp build/ggml/src/libggml.so ../LlamaBot/LlamaNative/Binaries/Cpu/libggml.so
+rm ../LlamaBot/LlamaNative/Binaries/Cpu/libggml.so.zip
+rm ../LlamaBot/LlamaNative/Binaries/Cpu/libggml.so.z01
+cp build/src/libllama.so ../LlamaBot/LlamaNative/Binaries/Cpu/libllama.so
+rm ../LlamaBot/LlamaNative/Binaries/Cpu/libllama.so.zip
+cd ../LlamaBot/LlamaNative/Binaries/Cpu
 zip -9 -s 95m libggml.so.zip libggml.so
 zip -9 libllama.so.zip libllama.so
 git add libggml.so.zip
