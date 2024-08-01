65f9cfb1ed6386027d4f080f0394e3a3fe674a37
Anthony Loffredo <loffredo.ap@gmail.com>
2024-07-31 23:22:40 -0700

    script

---
+++ update_llamabot_cuda.sh
@@ -0,0 +1,13 @@
+cp /media/mrjackspade/4TB/llama.cpp/build/ggml/src/libggml.so /media/mrjackspade/4TB/LlamaBot/LlamaNative/Binaries/CUDA/libggml.so
+rm /media/mrjackspade/4TB/LlamaBot/LlamaNative/Binaries/CUDA/libggml.so.zip
+rm /media/mrjackspade/4TB/LlamaBot/LlamaNative/Binaries/CUDA/libggml.so.z01
+cp /media/mrjackspade/4TB/llama.cpp/build/src/libllama.so /media/mrjackspade/4TB/LlamaBot/LlamaNative/Binaries/CUDA/libllama.so
+rm /media/mrjackspade/4TB/LlamaBot/LlamaNative/Binaries/CUDA/libllama.so.zip
+cd /media/mrjackspade/4TB/LlamaBot/LlamaNative/Binaries/CUDA
+zip -9 -s 95m libggml.so.zip libggml.so
+zip -9 libllama.so.zip libllama.so
+git add libggml.so.zip
+git add libggml.so.z01
+git add libllama.so.zip
+git commit -m "updating libraries"
+git push
