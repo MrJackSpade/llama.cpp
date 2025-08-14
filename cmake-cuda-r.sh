77152cb9f26e27c8ae679ed5b54591740ca74c84
Anthony Loffredo <loffredo.ap@gmail.com>
2025-08-13 20:53:19 -0700

    dh

--- cmake-cuda-r.sh
+++ cmake-cuda-r.sh
@@ -1,13 +1,14 @@
 cmake -S . -B build/ \
     -D CMAKE_BUILD_TYPE=Release \
-    -DGGML_CUDA=OFF \
+    -DGGML_CUDA=ON \
     -DGGML_AVX=ON \
     -DGGML_AVX2=ON \
     -DGGML_AVX512=ON \
     -DGGML_AVX512_VBMI=ON \
     -DGGML_AVX512_VNNI=ON \
     -DGGML_FMA=ON \
-    -DGGML_F16C=ON
+    -DGGML_F16C=ON \
+    -DLLAMA_CURL=OFF
 
 cmake --build build/ -- -j 8
 sudo find / | grep libggml.so
